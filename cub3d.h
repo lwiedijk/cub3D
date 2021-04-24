24a433e2e7fb2b17b603edca6dee25002a6c24f7
Laura Wiedijk <67950116+lwiedijk@users.noreply.github.com>
2021-04-24 09:05:47 +0200

    Add files via upload

---
+++ cub3d.h
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   cub3d.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lwiedijk <lwiedijk@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/03/19 14:03:21 by lwiedijk      #+#    #+#                 */
+/*   Updated: 2021/04/16 15:01:51 by lwiedijk      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+# define KOPINK "\033[38;5;200m"
+# define RES "\033[0m"
+
+typedef struct	s_mlx
+{
+	void		*mlx;
+	void		*win;
+	void		*img_1;
+	void		*img_2;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+	int			next_frame;
+	int			on_off;
+}				t_mlx;
+
+typedef struct	s_maze
+{
+	int			filepos;
+	int			screenres_y;
+	int			screenres_x;
+	char		*north_texture;
+	char		*south_texture;
+	char		*east_texture;
+	char		*west_texture;
+	char		*sprite;
+	int			floor_color;
+	int			ceiling_color;
+	int			r;
+	int			g;
+	int			b;
+	int			map_y;
+	int			*map_x;
+	int			**map;
+}				t_maze;
+
+typedef struct s_player
+{
+	int	pos_x;
+	int	pos_y;
+	int	walkdirection;
+	int	move_speed;
+}		t_player;
+
+typedef	struct	s_port
+{
+	t_mlx		*mlx;
+	t_maze		*blueprint;
+	t_player	*player;
+}				t_port;
+
+
+void	init_maze(t_maze *blueprint);
+void	init_mlx(t_mlx *mlx);
+void	init_player(t_player *player);
+void	parse(char *av, t_maze *blueprint);
+void	parse_mapfile(t_maze *blueprint, char *mapfile);
+void	parse_screenres(t_maze *blueprint, char *mapfile);
+void	parse_textures(t_maze *blueprint, char *mapfile, char texture_type);
+void	parse_color(t_maze *blueprint, char *mapfile, char type);
+void	parse_map(t_maze *blueprint, char *mapfile);
+int		x_button_hook(t_mlx *mlx);
+int		key_press_hook(int keycode, t_port *port);
+int		render_frame(t_port *port);
+
