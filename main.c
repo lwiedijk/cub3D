b668cdb5f55dd1cc0f08f24c52cc8e2bafe8d794
Laura Wiedijk <67950116+lwiedijk@users.noreply.github.com>
2021-04-28 00:27:36 +0200

    main cub

---
+++ main.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lwiedijk <lwiedijk@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/03/19 14:14:26 by lwiedijk      #+#    #+#                 */
+/*   Updated: 2021/04/17 09:59:04 by lwiedijk      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+#include "mlx/mlx.h"
+#include <stdio.h>
+
+void	set_player(t_port *port)
+{
+	int		x;
+	int 	y;
+
+	x = 0;
+	y = 0;
+	while(y < port->blueprint->map_y)
+	{
+		x = 0;
+		while (x < port->blueprint->map_x[y])
+		{
+			if (port->blueprint->map[y][x] == 'p')//player
+			{
+				port->player->pos_x = x;
+				port->player->pos_y = y;
+			}
+			x++;
+		}
+		y++;
+	}
+}
+
+void	render_loop(t_maze *blueprint, t_mlx *mlx, t_port *port)
+{
+	mlx->mlx = mlx_init();
+	mlx->win = mlx_new_window(mlx->mlx, blueprint->screenres_x, blueprint->screenres_y, "cub3D");
+	mlx_hook(mlx->win, 17, 1L<<17, &x_button_hook, mlx);
+	mlx_hook(mlx->win, 2, 1L<<0, &key_press_hook, port);
+	mlx_loop_hook(mlx->mlx, render_frame, port);
+	mlx_loop(mlx->mlx);
+}
+
+int	main(int ac, char **av)
+{
+	t_port	port;
+	t_maze	blueprint;
+	t_mlx	mlx;
+	t_player	player;
+
+	init_player(&player);
+	init_maze(&blueprint);
+	init_mlx(&mlx);
+	port.blueprint = &blueprint;
+	port.mlx = &mlx;
+	port.player = &player;
+	parse(av[1], &blueprint);
+	set_player(&port);
+	render_loop(&blueprint, &mlx, &port);
+
+}
