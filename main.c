e5e4972379a02dc7e1c0c0d8496621a3eeec47cb
Laura Wiedijk <67950116+lwiedijk@users.noreply.github.com>
2021-05-26 18:15:49 +0200

    texture update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lwiedijk <lwiedijk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/19 14:14:26 by lwiedijk      #+#    #+#                 */
-/*   Updated: 2021/05/22 15:46:27 by lwiedijk      ########   odam.nl         */
+/*   Updated: 2021/05/24 16:41:43 by lwiedijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 void	render_loop(t_maze *blueprint, t_mlx *mlx, t_port *port)
 {
 	mlx->mlx = mlx_init();
+	read_textures(port, blueprint, port->tex);
 	mlx->win = mlx_new_window(mlx->mlx, blueprint->screenres_x, blueprint->screenres_y, "cub3D");
 	mlx_hook(mlx->win, 17, 1L<<17, &x_button_hook, mlx);
 	mlx_hook(mlx->win, 2, 1L<<0, &key_press_hook, port);
@@ -32,13 +33,16 @@ int	main(int ac, char **av)
 	t_mlx		mlx;
 	t_player	player;
 	t_rays		rays;
+	t_tex		tex;
 
 	init_player(&player);
 	init_maze(&blueprint);
 	init_mlx(&mlx);
+	init_tex(&tex);
 	port.blueprint = &blueprint;
 	port.mlx = &mlx;
 	port.player = &player;
+	port.tex = &tex;
 	parse(av[1], &blueprint);
 	set_player(&port);
 	init_rays(&rays, &blueprint);
--- read_textures.c
+++ read_textures.c
@@ -6,7 +6,7 @@
 /*   By: lwiedijk <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/24 15:14:13 by lwiedijk      #+#    #+#                 */
-/*   Updated: 2021/05/26 11:43:10 by lwiedijk      ########   odam.nl         */
+/*   Updated: 2021/05/26 17:02:49 by lwiedijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ void	read_textures(t_port *port, t_maze *blueprint, t_tex *tex)
 	//if (!tex->tex_n)
 	//ft_error(2);
 	tex->addr_n = mlx_get_data_addr(tex->tex_n, &tex->bpp_n, &tex->ls_n, &tex->end_n);
+	tex->tex_e = mlx_xpm_file_to_image(port->mlx->mlx, blueprint->east_texture, &tex->x_e, &tex->y_e);
+	tex->addr_e = mlx_get_data_addr(tex->tex_e, &tex->bpp_e, &tex->ls_e, &tex->end_e);
 }
 
 void	calculate_textures(t_port *port, t_tex *tex, char map)
@@ -30,6 +32,11 @@ void	calculate_textures(t_port *port, t_tex *tex, char map)
 	if (map == 'N')
 	{
 		tex->step = 1.0 * tex->y_n / port->rays->wall_striphight;
+		tex->position = (port->rays->draw_start - port->blueprint->screenres_y / 2 + port->rays->wall_striphight / 2) * tex->step;
+	}
+	if (map == 'E')
+	{
+		tex->step = 1.0 * tex->y_e / port->rays->wall_striphight;
 		tex->position = (port->rays->draw_start - port->blueprint->screenres_y / 2 + port->rays->wall_striphight / 2) * tex->step;
 		
 	}
