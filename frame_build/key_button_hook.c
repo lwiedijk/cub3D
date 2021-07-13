23035c181e8171be2a93f8f32a08b76d1715bca9
Laura Wiedijk <67950116+lwiedijk@users.noreply.github.com>
2021-07-13 13:03:22 +0200

    key_defines

--- frame_build/key_button_hook.c
+++ frame_build/key_button_hook.c
@@ -6,7 +6,7 @@
 /*   By: lwiedijk <lwiedijk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/05 14:00:59 by lwiedijk      #+#    #+#                 */
-/*   Updated: 2021/07/09 16:39:03 by lwiedijk      ########   odam.nl         */
+/*   Updated: 2021/07/13 13:01:33 by lwiedijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,8 @@ int	key_press_hook(int keycode, t_port *port)
 		//free(blueprint->map_x); which is a 2d array with the count for x on each y
 		//free(map[y][x]); which is a 2d array with the actual map
 
+		//free(texture_array); which is a array of t_tex_array * 5 (paths are freed in read_texture())
+
 
 		//mlx_destroy_image(port->mlx->mlx, port->mlx->img_1);
 		// or is it img_2? which one is active?
@@ -41,17 +43,17 @@ int	key_press_hook(int keycode, t_port *port)
 		//system("leaks cub3D");
 		exit (0);
 	}
-	if (keycode == 13)
+	if (keycode == W_KEY)
 		port->player->walkdirection = 1;
-	if (keycode == 1)
+	if (keycode == S_KEY)
 		port->player->walkdirection = -1;
-	if (keycode == 0)
+	if (keycode == A_KEY)
 		port->player->strafe = 1;
-	if (keycode == 2)
+	if (keycode == D_KEY)
 		port->player->strafe = -1;
-	if (keycode == 124)
+	if (keycode == RIGHT_ARROW)
 		port->player->turndirection = 1;
-	if (keycode == 123)
+	if (keycode == LEFT_ARROW)
 		port->player->turndirection = -1;
 	port->mlx->on_off = 1;
 	return (0);
@@ -59,17 +61,17 @@ int	key_press_hook(int keycode, t_port *port)
 
 int	key_release_hook(int keycode, t_port *port)
 {
-	if (keycode == 13)
+	if (keycode == W_KEY)
 		port->player->walkdirection = 0;
-	if (keycode == 1)
+	if (keycode == S_KEY)
 		port->player->walkdirection = 0;
-	if (keycode == 0)
+	if (keycode == A_KEY)
 		port->player->strafe = 0;
-	if (keycode == 2)
+	if (keycode == D_KEY)
 		port->player->strafe = 0;
-	if (keycode == 124)
+	if (keycode == RIGHT_ARROW)
 		port->player->turndirection = 0;
-	if (keycode == 123)
+	if (keycode == LEFT_ARROW)
 		port->player->turndirection = 0;
 	port->mlx->on_off = 1;
 	return (0);
